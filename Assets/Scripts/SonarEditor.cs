1d47730e7e8de3c8af5fa1f82f6b53635ea696af
Ferran Martin Vilà <ferranmartinvila21@gmail.com>
2018-01-26 20:26:01 +0100

    Sonar editor added! :+1:

---
+++ Assets/Scripts/SonarEditor.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(SonarScript))]
+public class SonarEditor : Editor {
+
+    private void OnSceneGUI()
+    {
+        SonarScript sonar = (SonarScript)target;
+        Handles.color = Color.white;
+        Handles.DrawWireArc(sonar.transform.position, Vector3.forward, Vector3.up, 360, sonar.sonar_radius);
+    }
+}
