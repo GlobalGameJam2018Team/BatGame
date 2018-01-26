b990a7dd4c6443b38104781894326e058e8c9ffd
Andreu <Andreu.rbj@gmail.com>
2018-01-27 00:34:13 +0100

    sonar script update :last_quarter_moon:

--- Assets/Scripts/SonarScript.cs
+++ Assets/Scripts/SonarScript.cs
@@ -12,6 +12,8 @@ public class SonarScript : MonoBehaviour {
     public LayerMask target_mask;
     public LayerMask obstacle_mask;
     public float mesh_resolution;
+    public int edge_resolution_iteration;
+    public float edge_distance_limit;
     public MeshFilter view_mesh_filter;
     Mesh view_mesh;
     [HideInInspector]
@@ -67,18 +69,36 @@ public class SonarScript : MonoBehaviour {
         {
             angle_deg += transform.eulerAngles.y;
         }
-        return new Vector3(Mathf.Sin(angle_deg * Mathf.Deg2Rad), Mathf.Cos(angle_deg * Mathf.Deg2Rad), 0);
+        return new Vector3(Mathf.Sin(angle_deg * Mathf.Deg2Rad),  0, Mathf.Cos(angle_deg * Mathf.Deg2Rad));
     }
     void DrawFielOfView()
     {
         int stepCount = Mathf.RoundToInt(sonar_angle * mesh_resolution);
         float step_angle_size = sonar_angle / stepCount;
         List<Vector3> view_points = new List<Vector3>();
+        ViewCastInfo old_view_cast = new ViewCastInfo();
         for (int i = 0; i <= stepCount; i++)
         {
             float angle = transform.eulerAngles.y - sonar_angle / 2 + step_angle_size * i;
             ViewCastInfo new_view_cast = ViewCast(angle);
+            if(i>0)
+            {
+                bool edge_excede = Mathf.Abs(old_view_cast.distance - new_view_cast.distance) > edge_distance_limit;
+                if(old_view_cast.hit!=new_view_cast.hit||old_view_cast.hit&&new_view_cast.hit && edge_excede )
+                   {
+                    EdgeInfo edge = FindEdge(old_view_cast, new_view_cast);
+                    if(edge.point_A!=Vector3.zero)
+                    {
+                        view_points.Add(edge.point_A);
+                    }
+                    if (edge.point_B != Vector3.zero)
+                    {
+                        view_points.Add(edge.point_B);
+                    }
+                }
+            }
             view_points.Add(new_view_cast.point);
+            old_view_cast = new_view_cast;
         }
 
         int vertex_count = view_points.Count + 1;
@@ -119,6 +139,32 @@ public class SonarScript : MonoBehaviour {
 
         }
     }
+
+    EdgeInfo FindEdge(ViewCastInfo min_view_cast,ViewCastInfo max_view_cast)
+    {
+        float min_angle = min_view_cast.angle;
+        float max_angle = max_view_cast.angle;
+        Vector3 min_point = Vector3.zero;
+        Vector3 max_point = Vector3.zero;
+        for(int i=0;i<edge_resolution_iteration;i++)
+        {
+            float angle = (min_angle + max_angle) / 2;
+            ViewCastInfo new_view_cast = ViewCast(angle);
+            bool edge_excede = Mathf.Abs(min_view_cast.distance - max_view_cast.distance) > edge_distance_limit;
+
+            if (new_view_cast.hit==min_view_cast.hit&&!edge_excede)
+            {
+                min_angle = angle;
+                min_point = new_view_cast.point;
+            }
+            else
+            {
+                max_angle = angle;
+                max_point = new_view_cast.point;
+            }
+        }
+        return new EdgeInfo(min_point, max_point);
+    }
     public struct ViewCastInfo
     {
         public bool hit;
@@ -131,6 +177,17 @@ public class SonarScript : MonoBehaviour {
             point = _point;
             distance = _distance;
             angle = _angle;
+        }
+    }
+    public struct EdgeInfo
+    {
+        public Vector3 point_A;
+        public Vector3 point_B;
+
+        public EdgeInfo(Vector3 _point_A,Vector3 _point_B)
+        {
+            point_A = _point_A;
+            point_B = _point_B;
         }
     }
 }
