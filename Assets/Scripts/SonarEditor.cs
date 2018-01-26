f7d89f59d71b00318e7d5e3287292bf216468364
Ferran Martin Vilà <ferranmartinvila21@gmail.com>
2018-01-26 20:55:57 +0100

    Sonar logic improved! :tangerine:

--- Assets/Scripts/SonarEditor.cs
+++ Assets/Scripts/SonarEditor.cs
@@ -11,5 +11,21 @@ public class SonarEditor : Editor {
         SonarScript sonar = (SonarScript)target;
         Handles.color = Color.white;
         Handles.DrawWireArc(sonar.transform.position, Vector3.forward, Vector3.up, 360, sonar.sonar_radius);
+
+        Vector3 sonar_angle_A = sonar.DirFromAngle(-sonar.sonar_angle / 2, false);
+        sonar_angle_A.y = sonar_angle_A.z;
+        sonar_angle_A.z = 0;
+        Vector3 sonar_angle_B = sonar.DirFromAngle(sonar.sonar_angle / 2, false);
+        sonar_angle_B.y = sonar_angle_B.z;
+        sonar_angle_B.z = 0;
+
+        Handles.DrawLine(sonar.transform.position, sonar.transform.position + sonar_angle_A * sonar.sonar_radius);
+        Handles.DrawLine(sonar.transform.position, sonar.transform.position + sonar_angle_B * sonar.sonar_radius);
+
+        Handles.color = Color.red;
+        foreach(Transform visible_target in sonar.visible_targets)
+        {
+            Handles.DrawLine(sonar.transform.position, visible_target.position);
+        }
     }
 }
--- Assets/Scripts/SonarScript.cs
+++ Assets/Scripts/SonarScript.cs
@@ -1,15 +1,62 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Collections.Generic;
 
 public class SonarScript : MonoBehaviour {
 
     public float sonar_radius = 20;
+    [Range(0,360)]
     public float sonar_angle = 90;
-	
 
-    public Vector3 DirFromAngle(float angle_deg)
+    public LayerMask target_mask;
+    public LayerMask obstacle_mask;
+
+    [HideInInspector]
+    public List<Transform> visible_targets = new List<Transform>();
+
+    private void Start()
     {
+        StartCoroutine("FindTargetsWithDelay", 0.2f);
+    }
+    IEnumerator FindTargetsWithDelay(float delay)
+    {
+        while(true)
+        {
+            yield return new WaitForSeconds(delay);
+            FindVisibleTargets();
+        }
+    }
+
+    void FindVisibleTargets()
+    {
+        visible_targets.Clear();
+
+        Collider[] targets_in_view_rad = Physics.OverlapSphere(transform.position, sonar_radius, target_mask);
+
+        for(int k = 0; k < targets_in_view_rad.Length; k++)
+        {
+            Transform target = targets_in_view_rad[k].transform;
+
+            Vector3 dir_to_target = (target.position - transform.position).normalized;
+            if(Vector3.Angle(transform.forward, dir_to_target) < sonar_angle / 2)
+            {
+                float dist_to_target = Vector3.Distance(transform.position, target.position);
+
+                if(!Physics.Raycast(transform.position,dir_to_target,dist_to_target,obstacle_mask))
+                {
+                    visible_targets.Add(target);
+                }
+            }
+        }
+    }
+
+    public Vector3 DirFromAngle(float angle_deg, bool angle_is_global)
+    {
+        if(!angle_is_global)
+        {
+            angle_deg += transform.eulerAngles.y;
+        }
         return new Vector3(Mathf.Sin(angle_deg * Mathf.Deg2Rad), 0, Mathf.Cos(angle_deg * Mathf.Deg2Rad));
     }
 }
