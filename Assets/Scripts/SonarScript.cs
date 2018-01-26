235e83367427518d9239a4ec756b21720f6904fd
Andreu <Andreu.rbj@gmail.com>
2018-01-26 20:07:49 +0100

    SonarScript update

---
+++ Assets/Scripts/SonarScript.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SonarScript : MonoBehaviour {
+
+    public float sonar_radius = 20;
+    public float sonar_angle = 90;
+	
+
+    public Vector3 DirFromAngle(float angle_deg)
+    {
+        return new Vector3(Mathf.Sin(angle_deg * Mathf.Deg2Rad), 0, Mathf.Cos(angle_deg * Mathf.Deg2Rad));
+    }
+}
