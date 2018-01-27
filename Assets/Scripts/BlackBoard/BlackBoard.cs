8e14b25b0bc44a222c9f59c3c653ed1315c08e2f
mizquierdo97 <mizquierdo97@gmail.com>
2018-01-27 01:03:28 +0100

    SavePoints and BlackBoard

---
+++ Assets/Scripts/BlackBoard/BlackBoard.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlackBoard : MonoBehaviour {
+
+    public Vector2 initialPos = new Vector2(0, 0);
+    public Vector2 savePointPos = new Vector2(0, 0);
+    public bool savePointActive = false;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/Player/Lives.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lives : MonoBehaviour {
+
+    public int lives = 3;
+    public LayerMask walls;
+    public LayerMask enemies;
+    public BlackBoard bBoard;
+// Use this for initialization
+	void Start () {
+     
+	}
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.layer == 10 || collision.gameObject.layer ==11)
+        {
+            lives--;
+            if (!bBoard.savePointActive)
+                transform.position = new Vector3(bBoard.initialPos.x, 0, bBoard.initialPos.y);
+            else
+                transform.position = new Vector3(bBoard.savePointPos.x, 0, bBoard.savePointPos.y);
+            
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+      
+
+	}
+}
