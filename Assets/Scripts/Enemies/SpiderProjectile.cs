0d02eeea5e42d88d2f9610ba0f1665e64babaa69
marclafr <marc.lafrwy@gmail.com>
2018-01-27 03:01:27 +0100

    Spider Projectiles Slow the player && bug with disappearing projectiles solved

--- Assets/Scripts/Enemies/SpiderProjectile.cs
+++ Assets/Scripts/Enemies/SpiderProjectile.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class SpiderProjectile : MonoBehaviour
 {
     public float projectile_speed = 1.0f;
+    public float slow_decrease = 2.0f;
+    public float slow_time = 3.0f;
     public GameObject player;
 
     private Vector3 direction;
@@ -21,12 +23,11 @@ public class SpiderProjectile : MonoBehaviour
 
     void OnCollisionEnter(Collision collision)
     {
-        if (collision.gameObject.layer != 11)   //If its not an enemy
+        if (collision.gameObject.layer != 11 && collision.gameObject.layer != 0)   //If its not an enemy or default
         {
-            if (collision.gameObject.layer == 9)    //If its player
-            {
-                //TODO
-            }
+            if (collision.gameObject == player.gameObject)    //If its player
+                player.GetComponent<PlayerController2>().SlowDebuff(slow_decrease, slow_time);
+
             Object.Destroy(gameObject);
         }
     }
--- Assets/Scripts/PlayerController2.cs
+++ Assets/Scripts/PlayerController2.cs
@@ -6,11 +6,16 @@ public class PlayerController2 : MonoBehaviour {
 
     Rigidbody phys;
 
-    public float velocity = 0.1f;
-    public int force = 500;
-    
-	// Use this for initialization
-	void Start ()
+    public float horizontal_speed = 3.0f;
+    public float force = 500;
+
+    //Slowing Debuff
+    private bool slow_debuff = false;
+    private float slow_timer = 0.0f;
+    private float decrease_amount = 1.0f;
+
+    // Use this for initialization
+    void Start ()
     {
         Physics.gravity = new Vector3(0, 0, -9.81f);
         phys = GetComponent<Rigidbody>();
@@ -20,10 +25,11 @@ public class PlayerController2 : MonoBehaviour {
 	// Update is called once per frame
 	void Update ()
     {
-       /* float angle = Mathf.Atan2(phys.velocity.x, phys.velocity.y / 2.0f) * Mathf.Rad2Deg - 90;
+       /*
+        float angle = Mathf.Atan2(phys.velocity.x, phys.velocity.y / 2.0f) * Mathf.Rad2Deg - 90;
         gameObject.transform.eulerAngles = new Vector3(0, 0, -angle);
+        */
 
-*/
         if (Input.GetKeyDown("space"))
         {
             phys.velocity = new Vector3(phys.velocity.x, 0, phys.velocity.z);
@@ -32,14 +38,38 @@ public class PlayerController2 : MonoBehaviour {
      
         if (Input.GetKey(KeyCode.RightArrow))
         {
-            phys.velocity = new Vector3(3, 0, phys.velocity.z);
+            phys.velocity = new Vector3(horizontal_speed, 0, phys.velocity.z);
         }
        
         if (Input.GetKey(KeyCode.LeftArrow))
         {
-            phys.velocity = new Vector3(-3, 0, phys.velocity.z);
+            phys.velocity = new Vector3(-horizontal_speed, 0, phys.velocity.z);
         }
         transform.position = new Vector3(transform.position.x, 0, transform.position.z);
 
+        //Slowing debuff
+        if (slow_debuff)
+        {
+            if (slow_timer <= 0.0f)
+            {
+                force *= decrease_amount;
+                horizontal_speed *= decrease_amount;
+                slow_debuff = false;
+            }
+            slow_timer -= Time.deltaTime;
+        }
+        //--------------
+    }
+
+    public void SlowDebuff(float decrease, float time)
+    {
+        if (!slow_debuff)
+        {
+            force /= decrease;
+            horizontal_speed /= decrease;
+            decrease_amount = decrease;
+        }
+        slow_timer = time;
+        slow_debuff = true;
     }
 }
