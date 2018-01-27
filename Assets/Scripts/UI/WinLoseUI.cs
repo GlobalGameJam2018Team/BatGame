fd74950ca98e2561294630ee53cdd8b4eaf2e6f7
marclafr <marc.lafrwy@gmail.com>
2018-01-27 14:40:13 +0100

    Lose panel created

---
+++ Assets/Scripts/UI/WinLoseUI.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WinLoseUI : MonoBehaviour
+{
+    public GameObject lose_panel;
+
+    public void LostGame(GameObject killed_by)
+    {
+        lose_panel.SetActive(true);
+        if (killed_by.tag == "Spider")
+        {
+
+        }
+
+    }
+}
