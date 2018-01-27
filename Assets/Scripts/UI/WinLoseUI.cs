e45fdc94c926697eedf65fe22b9d9ef13b95cc27
marclafr <marc.lafrwy@gmail.com>
2018-01-27 16:51:44 +0100

    Lose panel finished

--- Assets/Scripts/UI/WinLoseUI.cs
+++ Assets/Scripts/UI/WinLoseUI.cs
@@ -15,10 +15,21 @@ public class WinLoseUI : MonoBehaviour
     public void LostGame(GameObject killed_by)
     {
         lose_panel.SetActive(true);
+        spider_img.SetActive(false);
+        bad_bat_img.SetActive(false);
+        snake_img.SetActive(false);
+        stalactite_img.SetActive(false);
+        spikes_img.SetActive(false);
+
         if (killed_by.tag == "Spider")
-        {
-
-        }
-
+            spider_img.SetActive(true);
+        else if (killed_by.tag == "EnemyBat")
+            bad_bat_img.SetActive(true);
+        else if (killed_by.tag == "Snake")
+            snake_img.SetActive(true);
+        else if (killed_by.tag == "Stalactite")
+            stalactite_img.SetActive(true);
+        else if (killed_by.tag == "Spikes")
+            spikes_img.SetActive(true);
     }
 }
