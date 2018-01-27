d1412a7ca94466db7e2a56c009b3417b6d7d41aa
marclafr <marc.lafrwy@gmail.com>
2018-01-27 15:56:47 +0100

    Added images for lose condition

--- Assets/Scripts/UI/WinLoseUI.cs
+++ Assets/Scripts/UI/WinLoseUI.cs
@@ -6,6 +6,12 @@ public class WinLoseUI : MonoBehaviour
 {
     public GameObject lose_panel;
 
+    public GameObject spider_img;
+    public GameObject bad_bat_img;
+    public GameObject snake_img;
+    public GameObject stalactite_img;
+    public GameObject spikes_img;
+
     public void LostGame(GameObject killed_by)
     {
         lose_panel.SetActive(true);
