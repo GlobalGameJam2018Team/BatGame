1036c5bfc6f0473bee14ca76ecaa9943a0daad98
Andreu <Andreu.rbj@gmail.com>
2018-01-27 17:26:39 +0100

    dead animation on player

--- Assets/Scripts/Player/Lives.cs
+++ Assets/Scripts/Player/Lives.cs
@@ -8,14 +8,16 @@ public class Lives : MonoBehaviour {
     public LayerMask walls;
     public LayerMask enemies;
     public BlackBoard bBoard;
-// Use this for initialization
-	void Start () {
-     
-	}
+    Animator anim;
+
+    // Use this for initialization
+    void Start () {
+        anim = GetComponent<Animator>();
+    }
 
     private void OnCollisionEnter(Collision collision)
     {
-        if (collision.gameObject.layer == 10 || collision.gameObject.layer == 11)
+        if (collision.gameObject.layer == 10 || collision.gameObject.layer == 11|| collision.gameObject.layer == 8)
         {
             lives--;
             if (lives < 0)
@@ -24,8 +26,9 @@ public class Lives : MonoBehaviour {
             }
             else
             {
-                Vector3 savePos = bBoard.savePoints[bBoard.lastPointActive].transform.position;
-                transform.position = new Vector3(savePos.x, 0, savePos.z);
+                anim.SetBool("dead", true);
+                
+          
             }
         }
 
@@ -33,7 +36,16 @@ public class Lives : MonoBehaviour {
 
     // Update is called once per frame
     void Update () {
-      
 
-	}
+       if( anim.GetBool("dead"))
+       {
+        if (anim.GetCurrentAnimatorStateInfo(0).IsName("dead"))
+        {
+                anim.SetBool("dead", false);
+
+                Vector3 savePos = bBoard.savePoints[bBoard.lastPointActive].transform.position;
+                transform.position = new Vector3(savePos.x, 0, savePos.z);
+            }
+        }
+    }
 }
